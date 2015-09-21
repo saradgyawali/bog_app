a189bf743e3618760da4a4e74efcc52a7d4c0316
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-09-21 00:04:03 -0700

    fourth commit

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,40 @@
+class CreaturesController < ApplicationController
+
+    def index
+        @creatures = Creature.all
+        render :index
+    end
+
+    def new
+        render :new
+    end 
+
+    def create
+        new_creature = params.require(:creature).permit(:name, :description)
+        Creature.create(new_creature)
+        redirect_to "/creatures"
+    end
+
+    def new
+        @creature = Creature.new
+        render :new
+    end
+
+    def show
+        id = params[:id]
+        @creature = Creature.find(id)
+        render :show
+    end
+    def create
+        new_creature = params.require(:creature).permit(:name, :description)
+        creature = Creature.create(new_creature)
+        redirect_to "/creatures/#{creature.id}"
+    end
+
+    def destroy
+        id = params[:id]
+        creature = Creature.find(id)
+        creature.destroy
+        redirect_to "/creatures"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,21 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+    root to: 'creatures#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    # Add prefixes to routes using `as: "some_prefix"` syntax
+    get '/creatures', to: 'creatures#index', as: "creatures"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    get '/creatures/new', to: 'creatures#new', as: "new_creature"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+    get '/creatures/:id', to: 'creatures#show', as: "creature"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    get '/creatures/:id/edit', to: 'creatures#show', as: "edit_creature"        
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+    post "/creatures", to: "creatures#create"
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+    # The update route
+    patch "/creatures/:id", to: "creatures#update"
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+    # the destroy route
+    delete "/creatures/:id", to: "creatures#destroy"
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
